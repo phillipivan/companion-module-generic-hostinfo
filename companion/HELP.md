d15355a5268c2c6c59b8193b51ebf118bc9a970a
Phillip Ivan Pietruschka <ivanpietruschka@gmail.com>
2024-06-18 22:19:44 +1000

    add variables from systemInformation update help.md update gitignore convert to ESM

--- companion/HELP.md
+++ companion/HELP.md
@@ -3,14 +3,21 @@
 Free memory, Load Average and Uptime updated every second. Load Average variables only reported on Linux.
 
 ## Variables
-- **Architecture** 
+- **Architecture**
+- **CPU Speed**
+- **FQDN**
 - **Free Memory**
+- **Free Memory (%)**
 - **Load Average: 1 Minute**
 - **Load Average: 5 Minute**
 - **Load Average: 15 Minute**
+- **Load Current**
 - **Machine**
+- **Module Uptime**
 - **Platform**
 - **Release**
+- **System Time**
+- **System Time Zone**
 - **Total Memory**
 - **Type**
 - **User Name**
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-const { InstanceBase, runEntrypoint, InstanceStatus } = require('@companion-module/base')
-const UpgradeScripts = require('./upgrades')
-const os = require('os')
+import { InstanceBase, runEntrypoint, InstanceStatus } from '@companion-module/base'
+import UpgradeScripts from './upgrades.js'
+import os from 'os'
+import si from 'systeminformation'
 
 const updateInterval = 1000
 
@@ -8,6 +9,7 @@ class Host_Info extends InstanceBase {
 	constructor(internal) {
 		super(internal)
 		this.updateTimer = {}
+		this.moduleUptime = 0
 	}
 
 	async init(config) {
@@ -16,6 +18,7 @@ class Host_Info extends InstanceBase {
 		this.updateVariableDefinitions()
 		this.updateVariableValues()
 	}
+
 	// When module gets deleted
 	async destroy() {
 		this.log('debug', 'destroy')
@@ -47,11 +50,32 @@ class Host_Info extends InstanceBase {
 	async updateChangingVariables() {
 		let vars = []
 		let loadavg = os.loadavg()
+		this.moduleUptime += 1
+		try {
+			//const cpu = await si.cpu();
+			const speed = await si.cpuCurrentSpeed() //avg
+			//const temp = await si.cpuTemperature()//main
+			const timeStamp = await si.time() //current, timezone
+			const time = new Date(timeStamp.current)
+			const load = await si.currentLoad() //avgLoad, currentLoad
+			vars['cpuSpeed'] = speed.avg
+			vars['loadCurrent'] = Math.round(load.currentLoad)
+			vars['systemTime'] = time
+			vars['systemTimeZone'] = timeStamp.timezone
+		} catch (e) {
+			console.log(e)
+			vars['cpuSpeed'] = 'unknown'
+			vars['cpuTemp'] = 'unknown'
+			vars['loadCurrent'] = 'unknown'
+			vars['systemTime'] = 'unknown'
+			vars['systemTimeZone'] ='unknown'
+		}
 		vars['freemem'] = os.freemem()
 		vars['freememPercent'] = Math.round(os.freemem() / os.totalmem() * 100)
 		vars['loadav_1'] = loadavg[0]
 		vars['loadav_5'] = loadavg[1]
 		vars['loadav_15'] = loadavg[2]
+		vars['moduleUptime'] = this.moduleUptime
 		vars['uptime'] = os.uptime()
 		this.setVariableValues(vars)
 		this.updateTimer = setTimeout(() => {
@@ -63,14 +87,35 @@ class Host_Info extends InstanceBase {
 		let vars = []
 		let loadavg = os.loadavg()
 		let userInfo = os.userInfo()
+		try {
+			//const cpu = await si.cpu();
+			const speed = await si.cpuCurrentSpeed() //avg
+			//const temp = await si.cpuTemperature()//main
+			const os = await si.osInfo()
+			const timeStamp = await si.time() //current, timezone
+			const load = await si.currentLoad() //avgLoad, currentLoad
+			const time = new Date(timeStamp.current)
+			vars['cpuSpeed'] = speed.avg
+			vars['fqdn'] = os.fqdn
+			vars['loadCurrent'] = Math.round(load.currentLoad)
+			vars['systemTime'] = time
+			vars['systemTimeZone'] = timeStamp.timezone
+		} catch (e) {
+			console.log(e)
+			vars['cpuSpeed'] = 'unknown'
+			vars['loadCurrent'] = 'unknown'
+			vars['systemTime'] = 'unknown'
+			vars['systemTimeZone'] ='unknown'
+		}
 		vars['arch'] = os.arch()
 		vars['freemem'] = os.freemem()
 		vars['freememPercent'] = Math.round(os.freemem() / os.totalmem() * 100)
 		vars['hostname'] = os.hostname()
-		vars['loadav_1'] = loadavg[0]
-		vars['loadav_5'] = loadavg[1]
-		vars['loadav_15'] = loadavg[2]
+		vars['loadAvg_1'] = loadavg[0]
+		vars['loadAvg_5'] = loadavg[1]
+		vars['loadAvg_15'] = loadavg[2]
 		vars['machine'] = os.machine()
+		vars['moduleUptime'] = this.moduleUptime
 		vars['platform'] = os.platform()
 		vars['release'] = os.release()
 		vars['totalmem'] = os.totalmem()
@@ -87,19 +132,25 @@ class Host_Info extends InstanceBase {
 	updateVariableDefinitions() {
 		this.setVariableDefinitions([
 			{ variableId: 'arch', name: 'Architecture' },
+			{ variableId: 'cpuSpeed', name: 'CPU Current Average Speed (GHz)' },
+			{ variableId: 'fqdn', name: 'Fully Qualified Domain Name' },
 			{ variableId: 'freemem', name: 'Free Memory (b)' },
 			{ variableId: 'freememPercent', name: 'Free Memory (%)' },
 			{ variableId: 'hostname', name: 'Host Name' },
-			{ variableId: 'loadav_1', name: 'Load Average: 1 Minute' },
-			{ variableId: 'loadav_5', name: 'Load Average: 5 Minute' },
-			{ variableId: 'loadav_15', name: 'Load Average: 15 Minute' },
+			{ variableId: 'loadAvg_1', name: 'Load Average: 1 Minute' },
+			{ variableId: 'loadAvg_5', name: 'Load Average: 5 Minute' },
+			{ variableId: 'loadAvg_15', name: 'Load Average: 15 Minute' },
+			{ variableId: 'loadCurrent', name: 'Load Current (%)' },
 			{ variableId: 'machine', name: 'Machine' },
+			{ variableId: 'moduleUptime', name: 'Module Uptime (s)' },
 			{ variableId: 'platform', name: 'Platform' },
 			{ variableId: 'release', name: 'Release' },
+			{ variableId: 'systemTime', name: 'System Time' },
+			{ variableId: 'systemTimeZone', name: 'System Time Zone' },
 			{ variableId: 'totalmem', name: 'Total Memory (b)' },
 			{ variableId: 'type', name: 'Type' },
 			{ variableId: 'user', name: 'User Name' },
-			{ variableId: 'uptime', name: 'Up Time (s)' },
+			{ variableId: 'uptime', name: 'Uptime (s)' },
 			{ variableId: 'version', name: 'Version' },
 		])
 	}
